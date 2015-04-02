a010434395e9e576bc7ffc31cb3f599f82e2507e
Kevin Davis <kevinskii.ps@gmail.com>
2015-04-01 23:03:52 -0700

    Initial commit

---
+++ FSharp.Utilities/Utilities.fs
@@ -0,0 +1,13 @@
+﻿/// Contains a set of extensions and other functions to support common tasks in F# data processing.
+module FSharp.Utilities
+
+/// <summary>
+/// Similar to F#'s Seq.groupBy function, but removes the key from the grouped values.
+/// </summary>
+let groupValuesBy (projection:'T->('Key*'Values)) (source:seq<'T>) =
+    // Split the elements into (key,values) tuples
+    Seq.map projection source
+    // Group by key
+    |> Seq.groupBy (fun (key,_) -> key)
+    // Remove the key from the grouped values 
+    |> Seq.map (fun (key,valsIncludingKey) -> key, valsIncludingKey |> Seq.map (fun (_,valsWithoutKey) -> valsWithoutKey))
